74f623bd1815674e75f7e57963b9f8cf6fb2e9df
Jose M. Torres <jmtorresr@gmail.com>
2020-10-09 17:17:28 +0900

    Tests passing for findNames
    Co-authored-by: dhequex <dhequex@users.noreply.github.com>
    Co-authored-by: ninamatti <ninamatti@users.noreply.github.com>

--- pokemonager.js
+++ pokemonager.js
@@ -3,25 +3,31 @@
     // This should return an array of all the names of n Pokemon from the Pokemon API.
     
     findNames(n) {
-     fetch(`https://pokeapi.co/api/v2/pokemon?${n}?limit=${n}`)
+     return fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${n}`)
       .then((Response) => {
         return Response.json();
 
       })
-      .then((json) =>{
+      .then((json) => {
         return json.results.map((jsonElement) => {
           return jsonElement["name"];
         });
       })
-      .then((arrayOfNames) => {
-        console.log(arrayOfNames);
-      });
   
     };
 
     // This should return an array of all the Pokemon that are under a particular weight.
 
     findUnderWeight(weight) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/1/?limit=10`)
+        .then((Response) =>{
+          return Response.json();
+        })
+        .then((json) => {
+          return json.results.map((pokemon) => {
+            console.log(pokemon);
+          })
+        })
       // Your code here.
       // ** LIMIT TO THE FIRST 10 POKEMON
       // We don't want to make too many unnecessary calls to the Pokemon API
